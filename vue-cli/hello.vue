f29bda5e729c3621570b0875e1a8136846402900
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-03 23:22:22 +0900

    hello vue-cli

---
+++ vue-cli/hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="message">メッセージ: {{ msg }}</p>
+</template>
+<script>
+export default {
+  data () {
+    return { msg: 'こんにちは!' }
+  }
+}
+</script>
+<style>
+.message { color: #42b983; }
+</style>
